f048ea141c97e2646b46e1430a6a54d3478a7072
Blaise143 <blaiseappol@gmail.com>
2024-01-21 00:57:48 -0800

    experiments

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from dataloaders import train_dataset
-print(train_dataset)
+from dataloaders import CustomDataLoader
+import torch
+from models import VariationalAutoEncoder
+import yaml
+
+with open("configs/config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+print(config)
+x = torch.randn((3, 3, 250, 250))
+print(x.shape)
+vae = VariationalAutoEncoder(config)
+# print(vae(x)[0].shape)
+# print(vae)
+dataloader = CustomDataLoader(400)
+print(len(dataloader.train_dataset))
+print(len(dataloader.val_dataset))
