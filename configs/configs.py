5b42f6ef32d9580a973a46e15192bdc4d57003d1
Blaise143 <blaiseappol@gmail.com>
2024-01-21 19:01:51 -0800

    removed unnecessary files

--- configs/configs.py
+++
@@ -1,9 +0,0 @@
-# LEARNING_RATE = 4e-3  # karpathy constant
-# DROPOUT = 0.4
-import yaml
-
-with open("configs/config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-print(config["model_config"]["encoder_layers"])
-print(config["train_config"])
-# print(type(config))
