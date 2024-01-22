2a94bf6c3776a55ea7d1e625809511b8baee3ad2
Blaise143 <blaiseappol@gmail.com>
2024-01-21 19:04:02 -0800

    added error handling

--- dataloaders/custom_dataset.py
+++ dataloaders/custom_dataset.py
@@ -2,10 +2,13 @@ import torch
 from torchvision import transforms, datasets
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
+from PIL import UnidentifiedImageError
+# from torchvision.datasets import ImageFolder
 
 
 class CustomDataLoader(LightningDataModule):
-    def __init__(self, batch_size: int,
+    def __init__(self,
+                 batch_size: int,
                  train_path: str = "data/musemart/dataset_updated/training_set",
                  val_path: str = "data/musemart/dataset_updated/validation_set") -> None:
         super().__init__()
@@ -18,12 +21,12 @@ class CustomDataLoader(LightningDataModule):
         self.train_path = train_path
         self.val_path = val_path
         self.batch_size = batch_size
-        self.train_dataset = datasets.ImageFolder(
+        self.train_dataset = RobustImageFolder(
             root=self.train_path,
             transform=transform
         )
-        self.val_dataset = datasets.ImageFolder(
-            root=val_path,
+        self.val_dataset = RobustImageFolder(
+            root=self.val_path,
             transform=transform
         )
 
@@ -31,7 +34,10 @@ class CustomDataLoader(LightningDataModule):
         dataloader = DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
-            shuffle=True
+            shuffle=True,
+            collate_fn=custom_collate_fn,
+            # num_workers=7
+
         )
         return dataloader
 
@@ -39,6 +45,26 @@ class CustomDataLoader(LightningDataModule):
         dataloader = DataLoader(
             self.val_dataset,
             batch_size=self.batch_size,
-            shuffle=False
+            collate_fn=custom_collate_fn,
+            shuffle=False,
+            # num_workers=7
         )
         return dataloader
+
+
+class RobustImageFolder(datasets.ImageFolder):
+    def __getitem__(self, index):
+        try:
+            return super().__getitem__(index)
+        except (IOError, OSError, UnidentifiedImageError) as e:
+            # print(
+            # f"Error encountered with image at index: {index}. Error: {e}. Skipping.")
+            return None
+
+
+def custom_collate_fn(batch):
+    batch = list(filter(lambda x: x is not None, batch))  # Remove None items
+    if len(batch) == 0:  # If all items are bad in a batch
+        return torch.tensor([]), torch.tensor([])
+    # Use the default collate function to combine the filtered items
+    return torch.utils.data.dataloader.default_collate(batch)
